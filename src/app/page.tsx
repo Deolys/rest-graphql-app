4b23587f5e7675e6c14baa8116baec55f915ba53
siloksan <tutel07@gmail.com>
2024-08-19 18:27:52 +0300

    feat: add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,16 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-      Main Page
+      Welcome page
       <ul>
         <li>
           <Link href="/login">Login page</Link>
         </li>
         <li>
           <Link href="/signup">Registration page</Link>
+        </li>
+        <li>
+          <Link href="/main">Main</Link>
         </li>
       </ul>
     </main>
