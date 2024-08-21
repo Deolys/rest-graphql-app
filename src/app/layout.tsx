c6ac0f3427daca3a1b82bc342815497dbbd8cf2e
Aleksei Osipov <alexey11rus@gmail.com>
2024-08-21 13:18:05 +0300

    feat: main page add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Layout } from 'antd'
+import Header from '@/components/header/header'
+import Footer from '@/components/footer/footer'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'REST/GraphiQL Client',
@@ -17,7 +17,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className="container">
+          <Layout>
+            <Header />
+            <main className="main">{children}</main>
+            <Footer />
+          </Layout>
+        </div>
+      </body>
     </html>
   )
 }
---
+++ src/components/footer/style.ts
@@ -0,0 +1,10 @@
+export const footerStyle: React.CSSProperties = {
+  display: 'flex',
+  paddingLeft: '20px',
+  paddingRight: '20px',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  color: '#000',
+  height: 60,
+  backgroundColor: '#f9f2ff',
+}
