77679602acb46357ccc27faf01bc037db338d8a4
k98940 <pv1xxb@yandex.ru>
2024-08-22 00:09:59 +0600

    chore: add client's constants

---
+++ src/constants/client.ts
@@ -0,0 +1,20 @@
+export const URLSegment = {
+  method: 2,
+  url: 3,
+}
+
+export const methods = {
+  get: 'GET',
+  post: 'POST',
+  put: 'PUT',
+  putch: 'PATCH',
+  delete: 'DELETE',
+  head: 'HEAD',
+  options: 'OPTIONS',
+}
+
+export const clientMenu = [
+  { label: 'Params', key: 'params' },
+  { label: 'Headers', key: 'headers' },
+  { label: 'Body', key: 'body' },
+]
