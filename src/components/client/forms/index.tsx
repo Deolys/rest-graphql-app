04a2060d4d1db27f8a6cbf2da9680ed49387dcb0
k98940 <pv1xxb@yandex.ru>
2024-08-23 02:44:15 +0600

    fix: imports

---
+++ src/components/client/forms/index.tsx
@@ -0,0 +1,15 @@
+import dynamic from 'next/dynamic'
+export { FormBody } from './body/body'
+import FormParams from './params/params'
+export { FormHeaders } from './headers/headers'
+
+// Для чего тут динамический импорт? - Чтобы отключить SSR для компоненты.
+// 'use client' не помогал и компонента рендерилась на сервере с ошибкой, т.к.
+// в ней используются клиентские хуки.
+export const Params = dynamic(
+  () => import('./params/params').then((form) => form),
+  {
+    loading: () => <p>Loading...</p>,
+    ssr: false,
+  },
+) as typeof FormParams
