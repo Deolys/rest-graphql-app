4c53aebc06fd5c87869402c2531d57eaf1297489
Deolys <ed03aleks@mail.ru>
2024-08-22 16:57:03 +0300

    feat: add error messages

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { JSX } from 'react';
-import { Button, Form, Input } from 'antd';
+import { Button, Form, Input, message } from 'antd';
 import type { FormProps } from 'antd';
 import Title from 'antd/es/typography/Title';
 import styles from '@/app/styles/auth-pages.module.css';
@@ -24,7 +24,7 @@ type FieldType = {
 
 export default function SignUpPage(): JSX.Element {
   const router = useRouter();
-  const [registerError, setRegisterError] = useState('');
+  const [messageApi, contextHolder] = message.useMessage();
   const [passwordValue, setPasswordValue] = useState('');
   const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
     const { name, email, password } = values;
@@ -35,93 +35,97 @@ export default function SignUpPage(): JSX.Element {
     const response = await registerWithEmailAndPassword(name, email, password);
     if (response?.error instanceof FirebaseError) {
       const errorMessage = getErrorByCodeFB(response.error.code);
-      setRegisterError(errorMessage);
+      messageApi.open({
+        type: 'error',
+        content: errorMessage,
+      });
     } else {
       router.push(pageRoutes.RESTFULL_CLIENT);
     }
   };
 
   return (
-    <div className={styles.formWrapper}>
-      <Title level={2} style={{ textAlign: 'center' }}>
-        Sign Up
-      </Title>
-      <Form
-        name="sign-up"
-        layout={'vertical'}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item<FieldType>
-          label="Name"
-          name="name"
-          rules={[{ required: true, message: 'Name is required' }]}
-          validateDebounce={700}
-          hasFeedback
+    <>
+      {contextHolder}
+      <div className={styles.formWrapper}>
+        <Title level={2} style={{ textAlign: 'center' }}>
+          Sign Up
+        </Title>
+        <Form
+          name="sign-up"
+          layout={'vertical'}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Email"
-          name="email"
-          rules={[
-            { required: true, message: 'Email is required' },
-            {
-              type: 'email',
-              message: 'Please enter a valid email',
-            },
-          ]}
-          validateDebounce={700}
-          hasFeedback
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[
-            { required: true, message: 'Password is required' },
-            () => ({
-              validator(_, value) {
-                setPasswordValue(value);
-                return rulesValidator(signUpPasswordRules, value);
+          <Form.Item<FieldType>
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: 'Name is required' }]}
+            validateDebounce={700}
+            hasFeedback
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Email"
+            name="email"
+            rules={[
+              { required: true, message: 'Email is required' },
+              {
+                type: 'email',
+                message: 'Please enter a valid email',
               },
-            }),
-          ]}
-          validateDebounce={700}
-          hasFeedback
-        >
-          <div>
+            ]}
+            validateDebounce={700}
+            hasFeedback
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[
+              { required: true, message: 'Password is required' },
+              () => ({
+                validator(_, value) {
+                  setPasswordValue(value);
+                  return rulesValidator(signUpPasswordRules, value);
+                },
+              }),
+            ]}
+            validateDebounce={700}
+            hasFeedback
+          >
+            <div>
+              <Input.Password />
+              <PasswordStrength password={passwordValue} />
+            </div>
+          </Form.Item>
+          <Form.Item<FieldType>
+            label="Confirm password"
+            name="confirmPassword"
+            rules={[
+              { required: true, message: 'Confirm your password' },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject('Passwords do not match');
+                },
+              }),
+            ]}
+            validateDebounce={700}
+            hasFeedback
+          >
             <Input.Password />
-            <PasswordStrength password={passwordValue} />
-          </div>
-        </Form.Item>
-        <Form.Item<FieldType>
-          label="Confirm password"
-          name="confirmPassword"
-          rules={[
-            { required: true, message: 'Confirm your password' },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject('Passwords do not match');
-              },
-            }),
-          ]}
-          validateDebounce={700}
-          hasFeedback
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item validateStatus="error" help={registerError}>
+          </Form.Item>
           <Button block type="primary" htmlType="submit">
             Submit
           </Button>
-        </Form.Item>
-        Already have an account? <a href={pageRoutes.SIGN_IN}>Login</a>
-      </Form>
-    </div>
+          Already have an account? <a href={pageRoutes.SIGN_IN}>Login</a>
+        </Form>
+      </div>
+    </>
   );
 }
