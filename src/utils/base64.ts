519f4d27e6af83e7ceb5fef22869f39e88a3322e
k98940 <pv1xxb@yandex.ru>
2024-08-23 14:25:13 +0600

    fix: cyrillic cause error in btoa()

--- src/utils/base64.ts
+++ src/utils/base64.ts
@@ -1,13 +1,12 @@
-// BUG Cyrillic cause error in btoa()
 export const base64 = {
-  encode: (str: string): string => (str ? btoa(str) : ''),
+  encode: (str: string): string => (str ? btoa(encodeURIComponent(str)) : ''),
   decode: (str64: string): string => {
     if (!str64) return '';
     let result = '';
     try {
-      result = atob(str64);
+      result = decodeURIComponent(atob(str64));
     } catch (error) {
-      return ''; // FIX catch error
+      console.warn('base64', error);
     }
 
     return result;
