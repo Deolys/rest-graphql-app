88862747d8e99f31e78cdf34cfcce50ab924c636
Deolys <ed03aleks@mail.ru>
2024-08-27 23:08:04 +0300

    refactor: add button titles

--- src/components/client/forms/body/body.tsx
+++ src/components/client/forms/body/body.tsx
@@ -2,9 +2,10 @@ import { jsonParseLinter } from '@codemirror/lang-json';
 import { lintGutter, linter } from '@codemirror/lint';
 import { Button } from 'antd';
 import Image from 'next/image';
-import { type JSX } from 'react';
+import { type JSX, useContext } from 'react';
 
 import { CodeEditor } from '@/components/code-editor';
+import { LanguageContext } from '@/providers/language';
 import { useAppDispatch } from '@/store/store';
 import type { ClientAction } from '@/types/client';
 import { prettifyJson } from '@/utils/prettify-json';
@@ -15,6 +16,7 @@ type Props = {
 };
 
 export function FormBody({ body, setBody }: Props): JSX.Element {
+  const { t } = useContext(LanguageContext);
   const dispatch = useAppDispatch();
   const handleBodyChange = (value: string): void => {
     dispatch(setBody(value));
@@ -34,6 +36,7 @@ export function FormBody({ body, setBody }: Props): JSX.Element {
       <Button
         onClick={handlePrettify}
         style={{ position: 'absolute', top: 0, right: 0 }}
+        title={t.prettify}
       >
         <Image width={30} height={30} src="/prettify-icon.svg" alt="prettify" />
       </Button>
