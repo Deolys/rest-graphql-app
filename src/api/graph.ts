c681123fd4a15c4970af9f9b1c08da7c8665716b
Aleksei Osipov <alexey11rus@gmail.com>
2024-08-29 12:47:55 +0300

    feat: graphQL fetch

---
+++ src/api/graph.ts
@@ -0,0 +1,42 @@
+type Props = {
+  endpointURL: string;
+  query: string;
+  headers: HeadersInit;
+};
+
+type FetchGraphReturn = Promise<{
+  body: string;
+  status: string;
+  error: string;
+}>;
+
+export async function fetchGraph({
+  endpointURL,
+  headers,
+  query,
+}: Props): FetchGraphReturn {
+  try {
+    const newURL = endpointURL.match(/(http:\/\/|https:\/\/)/i)
+      ? endpointURL
+      : 'https://' + endpointURL;
+    const correctURL = new URL(newURL);
+
+    const response = await fetch(correctURL, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ query }),
+    });
+
+    const status = `${response.status} ${response.ok ? 'OK' : 'HTTP error!'}`;
+
+    let responseBody = '';
+    if (response.headers.get('Content-Type')?.includes('application/json')) {
+      const json: unknown = await response.json();
+      responseBody = JSON.stringify(json);
+    }
+    return { body: responseBody, status, error: '' };
+  } catch (e) {
+    const error = e instanceof Error ? e.message : `${e}`;
+    return { body: '', status: '500 HTTP errror!', error };
+  }
+}
