984e81ee77350069b45d8ed4c43db36f3861ccad
Deolys <ed03aleks@mail.ru>
2024-08-31 11:48:48 +0300

    feat: add useDebouce hook

--- src/components/client/forms/variables/variables.tsx
+++ src/components/client/forms/variables/variables.tsx
@@ -1,8 +1,9 @@
 import { jsonParseLinter } from '@codemirror/lang-json';
 import { lintGutter, linter } from '@codemirror/lint';
-import { type JSX } from 'react';
+import { type JSX, useEffect, useState } from 'react';
 
 import { CodeEditor } from '@/components/code-editor';
+import { useDebounce } from '@/hooks/use-debounce';
 import { useAppDispatch } from '@/store/store';
 import type { ClientAction } from '@/types/client';
 
@@ -13,12 +14,20 @@ type Props = {
 
 export function FormVariables({ variables, setVariables }: Props): JSX.Element {
   const dispatch = useAppDispatch();
+  const [value, setValue] = useState(variables);
+  const debouncedValue = useDebounce(value, 500);
+
+  useEffect(() => {
+    dispatch(setVariables(debouncedValue));
+  }, [debouncedValue, dispatch, setVariables]);
+
   const handleVariablesChange = (value: string): void => {
-    dispatch(setVariables(value));
+    setValue(value);
   };
+
   return (
     <CodeEditor
-      value={variables}
+      value={value}
       onChange={handleVariablesChange}
       addExtensions={[linter(jsonParseLinter()), lintGutter()]}
     />
--- src/components/client/input-url/input-url.tsx
+++ src/components/client/input-url/input-url.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { Input } from 'antd';
-import { type ChangeEvent, type JSX } from 'react';
+import { type ChangeEvent, type JSX, useEffect, useState } from 'react';
 
+import { useDebounce } from '@/hooks/use-debounce';
 import { useAppDispatch } from '@/store/store';
 import type { ClientAction } from '@/types/client';
 
@@ -14,14 +15,20 @@ type Props = {
 
 export function InputUrl({ url, setURL, placeholder }: Props): JSX.Element {
   const dispatch = useAppDispatch();
+  const [value, setValue] = useState(url);
+  const debouncedValue = useDebounce(value, 500);
+
+  useEffect(() => {
+    dispatch(setURL(debouncedValue));
+  }, [debouncedValue, dispatch, setURL]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    dispatch(setURL(e.target.value));
+    setValue(e.target.value);
   };
 
   return (
     <Input
-      value={url}
+      value={value}
       onChange={handleChange}
       placeholder={placeholder}
     ></Input>
