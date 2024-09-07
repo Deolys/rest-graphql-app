59fae35468881a4c1cbb29b5e3e3ba7f2af0bfb0
Deolys <ed03aleks@mail.ru>
2024-09-07 22:17:41 +0300

    refactor: remove comment

--- src/components/client/forms/index.tsx
+++ src/components/client/forms/index.tsx
@@ -2,9 +2,6 @@ import dynamic from 'next/dynamic';
 
 import type CustomForm from './CustomForm/custom-form';
 
-// Для чего тут динамический импорт? - Чтобы отключить SSR для компоненты.
-// 'use client' не помогал и компонента рендерилась на сервере с ошибкой, т.к.
-// в ней используются клиентские хуки.
 export const ClientCustomForm = dynamic(
   () => import('./CustomForm/custom-form').then((form) => form),
   {
