c713e640493929b10132f025df0a5dc3ab4b6026
k98940 <pv1xxb@yandex.ru>
2024-09-09 13:42:43 +0600

    fix: error while send empty header

--- src/utils/headers-to-obj.ts
+++ src/utils/headers-to-obj.ts
@@ -1,9 +1,15 @@
 import type { DataType } from '@/types/client';
 
 export function headersToObj(headers: DataType[]): HeadersInit {
-  const mappedHeaders = headers.reduce<HeadersInit>(
-    (obj, header) => ({ ...obj, [header.keyName]: header.keyValue }),
-    {},
-  );
+  const filteredHeaders = headers.filter(noEmptyValues);
+  const mappedHeaders = filteredHeaders.reduce<HeadersInit>(removeKey, {});
   return mappedHeaders;
+}
+
+function noEmptyValues(header: DataType): boolean {
+  return Boolean(header.keyName) && Boolean(header.keyValue);
+}
+
+function removeKey(obj: HeadersInit, header: DataType): HeadersInit {
+  return { ...obj, [header.keyName]: header.keyValue };
 }
